81224e6358110d4212fed0b2316d0e66bd2ad585
Mohamed Ibrahim <sallamm733@gmail.com>
2024-11-19 15:15:34 +0200

    created GenerateBtn component with styled button and star icon
    GenerateBtn Component:
    Created GenerateBtn with a heading and a styled "Generate Images" button.
    Included a star icon from assets in the button.
    Styling:
    Applied responsive text sizes and hover effects for better user interaction.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header.jsx";
 import Steps from "../components/steps.jsx";
 import Description from "../components/Description.jsx";
 import Testimonials from "../components/Testimonials.jsx";
+import GenerateBtn from "../components/GenerateBtn.jsx";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Steps />
       <Description />
       <Testimonials />
+      <GenerateBtn />
     </div>
   );
 };
