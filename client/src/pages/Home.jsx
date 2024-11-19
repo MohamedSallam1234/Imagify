7362b8508f0cc954ef125b0a36ff2cf2f9575acd
Mohamed Ibrahim <sallamm733@gmail.com>
2024-11-19 14:10:14 +0200

    - Created `Description` component to explain the AI-powered text to image generator - Included sample images and detailed text descriptions - Styled the component with responsive design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.jsx";
 import Steps from "../components/steps.jsx";
+import Description from "../components/Description.jsx";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Steps />
+      <Description />
     </div>
   );
 };
